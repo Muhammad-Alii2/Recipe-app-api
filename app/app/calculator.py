b5ed5dcf1eeea736dd4025f6b50dff333d1141b2
Muhammad-Alii2 <142313809+Muhammad-Alii2@users.noreply.github.com>
2024-12-31 14:24:58 +0500

    Added Sample Tests, Docker Postgres db and Postgres db adaptor

---
+++ app/app/calculator.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add two numbers and returns the result
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract two numbers and returns the result
+    """
+
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,26 @@
+"""
+Testing calculator functions
+"""
+
+from django.test import SimpleTestCase
+from app import calculator
+
+class CalculatorTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """
+        Testing add function from calculator
+        """
+
+        result = calculator.add(1, 2)
+
+        self.assertEqual(result, 3)
+
+    def test_subtract_numbers(self):
+        """
+        Testing subtract function from calculator
+        """
+
+        result = calculator.subtract(5, 2)
+
+        self.assertEqual(result, 3)
